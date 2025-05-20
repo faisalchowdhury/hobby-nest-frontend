7db9f4e76e49ccd598c13d95f90768517b9fe066
Faisal Chowdhury <chowdhuryfaisal66@gmail.com>
2025-05-20 15:17:16 +0600

    Popular section UI created

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -60,6 +60,47 @@ const Home = () => {
           </p>
         </div>
       </div>
+
+      {/* Popular Hobbies */}
+
+      <div className="space-y-5">
+        <div className="space-y-3">
+          <h3 className="text-2xl">Popular Groups</h3>
+          <h1 className="text-6xl font-semibold">
+            Join the most active and inspiring hobby communities.
+          </h1>
+        </div>
+        <div className="popular-hobby grid grid-cols-2 gap-5">
+          <div className="bg-[#004122] text-white p-10 rounded-xl space-y-3">
+            <h2 className="text-2xl">Drawing & Painting Hub</h2>
+            <h1 className="text-3xl font-semibold">
+              Check the current status of Upwork services
+            </h1>
+            <button className="btn border-none bg-white rounded-md text-black hover:bg-black hover:text-white">
+              Join Now
+            </button>
+          </div>
+          <div className="bg-slate-200 text-black p-10 rounded-xl space-y-3">
+            <h2 className="text-2xl">Drawing & Painting Hub</h2>
+            <h1 className="text-3xl font-semibold">
+              Check the current status of Upwork services
+            </h1>
+            <button className="btn border-none bg-white rounded-md text-black hover:bg-black hover:text-white">
+              Join Now
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Why Us */}
+
+      <div className="grid grid-cols-3">
+        <div>
+          <div>
+            <img src="" alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -3,9 +3,105 @@ import React from "react";
 const Sidebar = () => {
   return (
     <div>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi repellat
-      quod neque quisquam itaque impedit eligendi quis odio minima nostrum,
-      blanditiis placeat maiores adipisci ratione quidem labore ex ullam ipsa.
+      <h2 className="text-2xl my-5">Group Admins</h2>
+      <div className="space-y-3">
+        <div className="flex gap-3 items-center">
+          <img
+            className="w-[30px] h-[30px] rounded-full"
+            src="https://i.ibb.co/xSr8Nh5Q/ben-den-engelsen-YUu9-UAc-OKZ4-unsplash.jpg"
+            alt=""
+          />
+          <p className="text-lg">Kamal Uddin</p>
+        </div>
+        <div className="flex gap-3 items-center">
+          <img
+            className="w-[30px] h-[30px] rounded-full"
+            src="https://i.ibb.co/xSr8Nh5Q/ben-den-engelsen-YUu9-UAc-OKZ4-unsplash.jpg"
+            alt=""
+          />
+          <p className="text-lg">Kamal Uddin</p>
+        </div>
+
+        <div className="flex gap-3 items-center">
+          <img
+            className="w-[30px] h-[30px] rounded-full"
+            src="https://i.ibb.co/xSr8Nh5Q/ben-den-engelsen-YUu9-UAc-OKZ4-unsplash.jpg"
+            alt=""
+          />
+          <p className="text-lg">Kamal Uddin</p>
+        </div>
+
+        <div className="flex gap-3 items-center">
+          <img
+            className="w-[30px] h-[30px] rounded-full"
+            src="https://i.ibb.co/xSr8Nh5Q/ben-den-engelsen-YUu9-UAc-OKZ4-unsplash.jpg"
+            alt=""
+          />
+          <p className="text-lg">Kamal Uddin</p>
+        </div>
+
+        <div className="flex gap-3 items-center">
+          <img
+            className="w-[30px] h-[30px] rounded-full"
+            src="https://i.ibb.co/xSr8Nh5Q/ben-den-engelsen-YUu9-UAc-OKZ4-unsplash.jpg"
+            alt=""
+          />
+          <p className="text-lg">Kamal Uddin</p>
+        </div>
+
+        <div className="flex gap-3 items-center">
+          <img
+            className="w-[30px] h-[30px] rounded-full"
+            src="https://i.ibb.co/xSr8Nh5Q/ben-den-engelsen-YUu9-UAc-OKZ4-unsplash.jpg"
+            alt=""
+          />
+          <p className="text-lg">Kamal Uddin</p>
+        </div>
+
+        <div className="flex gap-3 items-center">
+          <img
+            className="w-[30px] h-[30px] rounded-full"
+            src="https://i.ibb.co/xSr8Nh5Q/ben-den-engelsen-YUu9-UAc-OKZ4-unsplash.jpg"
+            alt=""
+          />
+          <p className="text-lg">Kamal Uddin</p>
+        </div>
+
+        <div className="flex gap-3 items-center">
+          <img
+            className="w-[30px] h-[30px] rounded-full"
+            src="https://i.ibb.co/xSr8Nh5Q/ben-den-engelsen-YUu9-UAc-OKZ4-unsplash.jpg"
+            alt=""
+          />
+          <p className="text-lg">Kamal Uddin</p>
+        </div>
+
+        <div className="flex gap-3 items-center">
+          <img
+            className="w-[30px] h-[30px] rounded-full"
+            src="https://i.ibb.co/xSr8Nh5Q/ben-den-engelsen-YUu9-UAc-OKZ4-unsplash.jpg"
+            alt=""
+          />
+          <p className="text-lg">Kamal Uddin</p>
+        </div>
+
+        <div className="flex gap-3 items-center">
+          <img
+            className="w-[30px] h-[30px] rounded-full"
+            src="https://i.ibb.co/xSr8Nh5Q/ben-den-engelsen-YUu9-UAc-OKZ4-unsplash.jpg"
+            alt=""
+          />
+          <p className="text-lg">Kamal Uddin</p>
+        </div>
+        <div className="flex gap-3 items-center">
+          <img
+            className="w-[30px] h-[30px] rounded-full"
+            src="https://i.ibb.co/xSr8Nh5Q/ben-den-engelsen-YUu9-UAc-OKZ4-unsplash.jpg"
+            alt=""
+          />
+          <p className="text-lg">Kamal Uddin</p>
+        </div>
+      </div>
     </div>
   );
 };
