b94d83a099c05b3bd87b34e5629b7a2cb0f9cde5
Faisal Chowdhury <chowdhuryfaisal66@gmail.com>
2025-05-22 14:45:35 +0600

    My Groups Page Created

--- src/Pages/MyGroups.jsx
+++ src/Pages/MyGroups.jsx
@@ -1,59 +1,87 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../Context/AuthContext";
+import { FaEdit } from "react-icons/fa";
+import { MdDeleteOutline } from "react-icons/md";
 const MyGroups = () => {
   const { user } = useContext(AuthContext);
+  const [groups, setGroups] = useState([]);
   console.log(user?.email);
   useEffect(() => {
-    fetch(`http:localhost:3000/user/${user?.email}`)
+    fetch(`http://localhost:3000/my-groups/${user?.email}`)
       .then((res) => res.json())
-      .then((data) => console.log(data));
-  }, []);
+      .then((data) => setGroups(data));
+  }, [user]);
+
+  // Handle delete ;
+
+  const handleDelete = (id) => {
+    fetch(`http://localhost:3000/delete/${id}`);
+  };
   return (
-    <div>
+    <div className="my-5">
       <div className="overflow-x-auto">
         <table className="table">
           {/* head */}
           <thead>
             <tr>
               <th>No</th>
-              <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
-              <th></th>
+              <th>Group Name</th>
+              <th>Meetup Info</th>
+              <th>Category</th>
+              <th>Action</th>
             </tr>
           </thead>
           <tbody>
             {/* row 1 */}
-            <tr>
-              <th>1</th>
-              <td>
-                <div className="flex items-center gap-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle h-12 w-12">
-                      <img
-                        src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-                        alt="Avatar Tailwind CSS Component"
-                      />
+
+            {groups.map((group, i) => (
+              <tr key={group._id}>
+                <th>{i + 1}</th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img src={group.image_url} alt={group.group_name} />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold text-xl">
+                        {group.group_name}
+                      </div>
+                      <div className="text-sm opacity-50">
+                        End Date : {group.end_date}
+                      </div>
                     </div>
                   </div>
-                  <div>
-                    <div className="font-bold">Hart Hagerty</div>
-                    <div className="text-sm opacity-50">United States</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Zemlak, Daniel and Leannon
-                <br />
-                <span className="badge badge-ghost badge-sm">
-                  Desktop Support Technician
-                </span>
-              </td>
-              <td>Purple</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
+                </td>
+                <td>
+                  {group.location}
+                  <br />
+                  <span className="badge badge-ghost badge-sm bg-green-900 text-white">
+                    Max Member : {group["max-member"]}
+                  </span>
+                </td>
+                <td>{group["hobby-category"]}</td>
+                <th className="space-x-3">
+                  <button className=" border-none btn-xs">
+                    <FaEdit
+                      color="gray"
+                      title="Edit this Group information"
+                      size={26}
+                    />
+                  </button>
+                  <button
+                    onClick={() => handleDelete(group._id)}
+                    className=" border-none btn-xs">
+                    <MdDeleteOutline
+                      color="red"
+                      title="Are you want to delete this ?"
+                      size={26}
+                    />
+                  </button>
+                </th>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
