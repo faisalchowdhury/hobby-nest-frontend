6c64ece1acb1bf8d3d3fef3bba83dd5c4ef98087
Faisal Chowdhury <chowdhuryfaisal66@gmail.com>
2025-05-23 12:34:49 +0600

    Minor responsiveness fixed

--- src/Component/GroupCard.jsx
+++ src/Component/GroupCard.jsx
@@ -6,21 +6,25 @@ const GroupCard = ({ group }) => {
     <div>
       <div
         key={group._id}
-        className=" rounded-lg  bg-slate-100 shadow-lg hover:shadow-xl hover:-translate-y-1 transition-all space-y-2">
+        className=" rounded-lg  bg-slate-100 shadow-lg hover:shadow-xl hover:-translate-y-1 transition-all space-y-3 ">
         <img
           className="w-full h-[200px] object-cover rounded-t-lg"
           src={group.image_url}
           alt=""
         />
-        <div className="p-5">
+        <div className="p-5 space-y-2">
           <h2 className="text-2xl">{group.group_name}</h2>
           <p>End Date : {group.end_date}</p>
+          <span className="bg-primary text-white px-2">
+            {" "}
+            Group Category : {group["hobby-category"]}
+          </span>
           <p>Description :{group.description.slice(0, 130)}...</p>
 
           <Link
             to={`/group/${group._id}`}
             className="btn bg-primary text-white hover:bg-black">
-            View Group
+            See More
           </Link>
         </div>
       </div>
